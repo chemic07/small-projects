d07332688cd2bee30ef1d5f01d1707f24b929ce9
Raj lodhi <rajlodhi1208@gmail.com>
2023-11-25 18:58:46 +0530

    made Quiz web app with 30  GK questions

---
+++ Quiz web app/dataset.js
@@ -0,0 +1,272 @@
+export const questions = [
+  {
+    question: "What is the capital of Germany?",
+    answers: [
+      { text: "Berlin", correct: true },
+      { text: "Madrid", correct: false },
+      { text: "Paris", correct: false },
+      { text: "Rome", correct: false },
+    ],
+  },
+  {
+    question: "What is the largest river in the world?",
+    answers: [
+      { text: "Nile", correct: false },
+      { text: "Amazon", correct: true },
+      { text: "Yangtze", correct: false },
+      { text: "Mississippi", correct: false },
+    ],
+  },
+  {
+    question: "In which year did the United States declare its independence?",
+    answers: [
+      { text: "1776", correct: true },
+      { text: "1789", correct: false },
+      { text: "1800", correct: false },
+      { text: "1812", correct: false },
+    ],
+  },
+  {
+    question: "Which planet is known as the 'Red Planet'?",
+    answers: [
+      { text: "Venus", correct: false },
+      { text: "Mars", correct: true },
+      { text: "Jupiter", correct: false },
+      { text: "Saturn", correct: false },
+    ],
+  },
+  {
+    question: "What is the largest ocean on Earth?",
+    answers: [
+      { text: "Atlantic", correct: false },
+      { text: "Indian", correct: false },
+      { text: "Southern", correct: false },
+      { text: "Pacific", correct: true },
+    ],
+  },
+  {
+    question: "Who developed the theory of general relativity?",
+    answers: [
+      { text: "Isaac Newton", correct: false },
+      { text: "Galileo Galilei", correct: false },
+      { text: "Albert Einstein", correct: true },
+      { text: "Stephen Hawking", correct: false },
+    ],
+  },
+  {
+    question: "What is the currency of Japan?",
+    answers: [
+      { text: "Yuan", correct: false },
+      { text: "Won", correct: false },
+      { text: "Yen", correct: true },
+      { text: "Ringgit", correct: false },
+    ],
+  },
+  {
+    question: "Which country is known as the Land of the Rising Sun?",
+    answers: [
+      { text: "China", correct: false },
+      { text: "South Korea", correct: false },
+      { text: "Japan", correct: true },
+      { text: "Vietnam", correct: false },
+    ],
+  },
+  {
+    question: "What is the official language of Brazil?",
+    answers: [
+      { text: "Portuguese", correct: true },
+      { text: "Spanish", correct: false },
+      { text: "Italian", correct: false },
+      { text: "French", correct: false },
+    ],
+  },
+  {
+    question: "Which mountain range separates Europe from Asia?",
+    answers: [
+      { text: "Rocky Mountains", correct: false },
+      { text: "Himalayas", correct: false },
+      { text: "Alps", correct: false },
+      { text: "Ural Mountains", correct: true },
+    ],
+  },
+  {
+    question: "What is the largest mammal in the world?",
+    answers: [
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+      { text: "Blue Whale", correct: true },
+      { text: "Rhinoceros", correct: false },
+    ],
+  },
+  {
+    question: "In which year did the Titanic sink?",
+    answers: [
+      { text: "1912", correct: true },
+      { text: "1905", correct: false },
+      { text: "1920", correct: false },
+      { text: "1931", correct: false },
+    ],
+  },
+  {
+    question: "What is the capital of Australia?",
+    answers: [
+      { text: "Sydney", correct: false },
+      { text: "Melbourne", correct: false },
+      { text: "Canberra", correct: true },
+      { text: "Brisbane", correct: false },
+    ],
+  },
+  {
+    question: "Which gas do plants absorb during photosynthesis?",
+    answers: [
+      { text: "Oxygen", correct: false },
+      { text: "Carbon Dioxide", correct: true },
+      { text: "Nitrogen", correct: false },
+      { text: "Hydrogen", correct: false },
+    ],
+  },
+  {
+    question: "What is the currency of Russia?",
+    answers: [
+      { text: "Euro", correct: false },
+      { text: "Ruble", correct: true },
+      { text: "Hryvnia", correct: false },
+      { text: "Tenge", correct: false },
+    ],
+  },
+  {
+    question: "Which famous playwright wrote 'Romeo and Juliet'?",
+    answers: [
+      { text: "William Wordsworth", correct: false },
+      { text: "William Faulkner", correct: false },
+      { text: "William Shakespeare", correct: true },
+      { text: "Oscar Wilde", correct: false },
+    ],
+  },
+  {
+    question: "What is the largest desert in the world?",
+    answers: [
+      { text: "Sahara Desert", correct: true },
+      { text: "Gobi Desert", correct: false },
+      { text: "Antarctic Desert", correct: false },
+      { text: "Arctic Desert", correct: false },
+    ],
+  },
+  {
+    question: "Which element has the chemical symbol 'O'?",
+    answers: [
+      { text: "Oxygen", correct: true },
+      { text: "Gold", correct: false },
+      { text: "Iron", correct: false },
+      { text: "Silver", correct: false },
+    ],
+  },
+  {
+    question: "What is the capital of Canada?",
+    answers: [
+      { text: "Toronto", correct: false },
+      { text: "Vancouver", correct: false },
+      { text: "Ottawa", correct: true },
+      { text: "Montreal", correct: false },
+    ],
+  },
+  {
+    question: "In which country is the Great Barrier Reef located?",
+    answers: [
+      { text: "Brazil", correct: false },
+      { text: "Australia", correct: true },
+      { text: "Thailand", correct: false },
+      { text: "Mexico", correct: false },
+    ],
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    answers: [
+      { text: "Vincent van Gogh", correct: false },
+      { text: "Pablo Picasso", correct: false },
+      { text: "Leonardo da Vinci", correct: true },
+      { text: "Michelangelo", correct: false },
+    ],
+  },
+  {
+    question: "What is the largest moon in our solar system?",
+    answers: [
+      { text: "Europa", correct: false },
+      { text: "Titan", correct: false },
+      { text: "Ganymede", correct: true },
+      { text: "Io", correct: false },
+    ],
+  },
+  {
+    question: "Which continent is the least populous?",
+    answers: [
+      { text: "Asia", correct: false },
+      { text: "Antarctica", correct: false },
+      { text: "Australia", correct: false },
+      { text: "South America", correct: true },
+    ],
+  },
+  {
+    question: "What is the capital of South Africa?",
+    answers: [
+      { text: "Johannesburg", correct: false },
+      { text: "Cape Town", correct: false },
+      { text: "Pretoria", correct: true },
+      { text: "Durban", correct: false },
+    ],
+  },
+  {
+    question: "Which famous scientist developed the laws of motion?",
+    answers: [
+      { text: "Galileo Galilei", correct: false },
+      { text: "Isaac Newton", correct: true },
+      { text: "Albert Einstein", correct: false },
+      { text: "Stephen Hawking", correct: false },
+    ],
+  },
+  {
+    question: "What is the primary ingredient in guacamole?",
+    answers: [
+      { text: "Tomatoes", correct: false },
+      { text: "Avocado", correct: true },
+      { text: "Onions", correct: false },
+      { text: "Peppers", correct: false },
+    ],
+  },
+  {
+    question: "In which year did World War I begin?",
+    answers: [
+      { text: "1914", correct: true },
+      { text: "1918", correct: false },
+      { text: "1939", correct: false },
+      { text: "1941", correct: false },
+    ],
+  },
+  {
+    question: "What is the largest bird in the world?",
+    answers: [
+      { text: "Penguin", correct: false },
+      { text: "Ostrich", correct: true },
+      { text: "Eagle", correct: false },
+      { text: "Albatross", correct: false },
+    ],
+  },
+  {
+    question: "Which planet is known as the 'Blue Planet'?",
+    answers: [
+      { text: "Mars", correct: false },
+      { text: "Earth", correct: true },
+      { text: "Neptune", correct: false },
+      { text: "Uranus", correct: false },
+    ],
+  },
+  {
+    question: "What is the capital of Argentina?",
+    answers: [
+      { text: "Rio de Janeiro", correct: false },
+      { text: "Buenos Aires", correct: true },
+      { text: "Santiago", correct: false },
+      { text: "Lima", correct: false },
+    ],
+  },
+];
